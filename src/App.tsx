75b72398265a9a77d01acc6bedd03972e1892649
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-07-29 08:07:10 +1000

    Edit comments.

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,9 @@ import styled from "styled-components";
 // 3. When user clicks on a city, get the current weather status + forecast for the next 5 days for that city then store this in local state. (DONE)
 // 4. Display the weather status and forecast for the next 5 days using something like the Google Weather UI (i.e. display current temp + conditions + max/min temp + common condition of each day over next 5 days) (DONE)
 // 5. Write jest unit tests for any functions.
-// 6. Write component test at the top-level component using React-Testing-Library and Mock-Service-Worker to mock API responses (testing both positive + negative paths).
+// 6. Write component test at the top-level component using React-Testing-Library and Mock-Service-Worker to mock API responses (testing both positive + negative paths). (DONE)
 // 7. Explain why cypress e2e test would be overkill here (e.g. reliance on 3rd api, not much we can assert on that Component test hasn't covered).
 // 8. Add Cypress visual test or explain why not done?
-
-// City object with unneeded properties removed.
 
 const Container = styled.div`
   background-color: #282c34;
@@ -44,6 +42,7 @@ const Title = styled.div`
   text-align: center;
 `;
 type FilteredCity = Pick<City, "name" | "coordinates">;
+
 function App() {
   const [searchName, setSearchName] = useState("");
 
