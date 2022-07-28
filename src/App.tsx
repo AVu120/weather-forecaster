dcbfdedc2dc51ba1d264746734c91052d3b01ffb
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-07-29 07:13:28 +1000

    Remove error when requerying weather forecase + remove unnecessary local state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import CitySearchBar from "components/CitySearchBar";
 import { City } from "types/city";
-import { getCities, mockedGetCitiesApiResponse } from "services/city";
+import { getCities } from "services/city";
 import { getWeatherForecast } from "services/weather";
 import { FilteredListItem, FilteredWeatherForecast } from "types/weather";
 import LoadingSpinner from "components/LoadingSpinner";
@@ -16,14 +16,14 @@ import styled from "styled-components";
 // days.
 
 // TODO: Write an application that gives you the weather forecast for any searchable city.
-// 1. Do mobile-first design.
-// 2. Add autocomplete component which onMount, calls API to get a list of searchable cities (total of 50) and stores in local state (autocomplete field populates with the loaded cities list).
-// 3. When user clicks on a city, get the current weather status + forecast for the next 5 days for that city then store this in local state.
-// 4. Display the weather status and forecast for the next 5 days using something like the Google Weather UI (i.e. display current temp + conditions + max/min temp + common condition of each day over next 5 days)
+// 1. Do mobile-first design. (DONE)
+// 2. Add autocomplete component which onMount, calls API to get a list of searchable cities (total of 50) and stores in local state (autocomplete field populates with the loaded cities list). (DONE)
+// 3. When user clicks on a city, get the current weather status + forecast for the next 5 days for that city then store this in local state. (DONE)
+// 4. Display the weather status and forecast for the next 5 days using something like the Google Weather UI (i.e. display current temp + conditions + max/min temp + common condition of each day over next 5 days) (DONE)
 // 5. Write jest unit tests for any functions.
 // 6. Write component test at the top-level component using React-Testing-Library and Mock-Service-Worker to mock API responses (testing both positive + negative paths).
 // 7. Explain why cypress e2e test would be overkill here (e.g. reliance on 3rd api, not much we can assert on that Component test hasn't covered).
-// 8. Add Cypress visual test
+// 8. Add Cypress visual test or explain why not done?
 
 // City object with unneeded properties removed.
 
@@ -51,9 +51,6 @@ function App() {
     FilteredWeatherForecast | undefined
   >();
 
-  const [weatherStatus, setWeatherStatus] = useState<
-    FilteredListItem | undefined
-  >();
   const [cities, setCities] = useState<FilteredCity[]>([]);
   const [isLoadingCities, setIsLoadingCities] = useState(false);
   const [isLoadingWeatherForecast, setIsLoadingWeatherForecast] =
@@ -67,7 +64,6 @@ function App() {
     try {
       setIsLoadingCities(true);
       const cities = await getCities();
-      // const cities = mockedGetCitiesApiResponse;
       const filteredCities = cities.map((city) => ({
         name: city.name,
         coordinates: city.coordinates,
@@ -82,6 +78,7 @@ function App() {
 
   const fetchWeatherForecast = async (latitude: number, longitude: number) => {
     try {
+      if (error) setError(undefined);
       setIsLoadingWeatherForecast(true);
       const weatherForecast = await getWeatherForecast(latitude, longitude);
       const filteredWeatherForecast = {
@@ -98,7 +95,6 @@ function App() {
         ),
       };
       setWeatherForecast(filteredWeatherForecast);
-      setWeatherStatus(filteredWeatherForecast.list[0]);
       setSelectedDate(filteredWeatherForecast.list[0].dateTime.slice(0, 10));
     } catch (error) {
       setError("Error: failed to fetch weather forecast.");
@@ -146,9 +142,12 @@ function App() {
         <LoadingSpinner />
       ) : (
         <>
-          {weatherStatus && <WeatherStatus weatherStatus={weatherStatus} />}
           {weatherForecast && selectedDate && (
             <>
+              <WeatherStatus
+                weatherStatus={weatherForecast.list[0]}
+                cityName={weatherForecast?.city.name}
+              />
               <FiveDayForecast
                 weatherForecast={weatherForecast}
                 onSelectDay={changeSelectedDate}
